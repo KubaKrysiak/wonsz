c58434eb82b3dc76510dc2dfcba3b4b731b44ed3
KubaKrysiak <43858173+KubaKrysiak@users.noreply.github.com>
2019-11-28 19:44:53 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-/**
- * @type {HTMLElement}
  */
 const wrap = document.querySelector("#wrap");
 /**
@@ -11,89 +9,92 @@ const ui = document.querySelector("#ui");
  */
 const canvas = document.querySelector("#game");
 const ctx = canvas.getContext("2d");
-
 const height = canvas.height = canvas.offsetHeight;
-const width = canvas.width = canvas.offsetWidth;
-
+const width = canvas.width = canvas.offsetWidth; 
 const cellSize = 20; // in pixels
-
 const offsetX = Math.floor((width % cellSize) / 2);
 const offsetY = Math.floor((height % cellSize) / 2);
 
 const board = {
 	width: Math.floor(width / cellSize),
 	height: Math.floor(height / cellSize),
-	render: ()=>{
+	render: () => {
 		ctx.fillStyle="black";
-		ctx.fillRect(0,0,width,height)
-		ctx.fillStyle="white"
-		ctx.fillStyle(offsetX,offsetY,cellSize*board.width,cellSize*board.height);
+		ctx.fillRect(0,0,width,height);
+		ctx.fillStyle="white";
+		ctx.fillRect(offsetX,offsetY,cellSize*board.width,cellSize*board.height);
 	}
 }
-
 const snake = {
-	sections:[],
-	direction:'right',
-	addHead:(x,y)=>{
+	sections: [],
+	direction: 'right',
+	addHead: (x, y) => {
 		snake.sections = [Section(x,y)].concat(snake.sections);
 		if(snake.sections.length > 1) snake.sections[1].head = false;
 	},
-	removeTail(){
-		if(snake.sections.length>1) return snake.sections.pop();
-		return false;
+	removeTail() {
+		if(snake.sections.length>1) snake.sections.pop();
 	},
-	render:()=>{
+	render: () => {
 		snake.sections.forEach(renderSection);
 	}
 }
-function Section(x,y) {
+function Section(x, y) {
 	return {
 		x,
 		y,
-		head:true
+		head: true
 	}
 }
-function renderSection(section){
-	/* tu trzeba narysowac moduł */ 
-	ctx.fillStyle= "black";
+function renderSection(section) {
+	/* tu trzeba narysowac moduł */
+	ctx.fillStyle = "black";
 	ctx.fillRect(	offsetX+section.x*cellSize,
-					offsetY+section.y*cellSize,
-					cellSize,
-					cellSize);
+						offsetY+section.y*cellSize,
+						cellSize,
+						cellSize );
 }
 function move() {
-	let {x,y} = snake.sections[0];
-	if(sneak.direction === 'right')x++;
-	if(sneak.directx === 'left')x--;
-	if(sneak.direction === 'down')y++;
-	if(sneak.direction === 'up')y--;
-	snake.addHead(x,y)
+	let { x, y } = snake.sections[0];
+	if(snake.direction === 'right') x++;
+	if(snake.direction === 'left') x--;
+	
+	if(snake.direction === 'down') y++;
+	if(snake.direction === 'up') y--;
+	
+	for (const el of snake.sections){
+		if(el.x === x && el.y === y) gameover();
+		
+	}
+	if(x<0) gameover();
+	// dodać głowę
+	snake.addHead(x,y);
 
+	// usunąć ogon
 	snake.removeTail();
-
 }
 
 const stats = {
-	speed: 1, // cells per second
+	speed: 5, // cells per second
 	points: 0
 }
 
 let timeoutID = 0;
-function step(){
+function step() {
+	// przesunąć wonsza
 	move()
-
+	// narysować planszę
 	board.render()
-
+	// narysować wonsza
 	snake.render()
-
-	setTimeout(step,1000*(1/stats.speed))
+	// zrobic timeout
+	 timeoutID = setTimeout(step,1000*(1/stats.speed))
 
 }
 
-function init(){
+function init() {
 	let x = Math.floor(board.width/2);
-	let x = Math.floor(board.height/2);
-
+	let y = Math.floor(board.height/2)
 	snake.addHead(x-4,y);
 	snake.addHead(x-3,y);
 	snake.addHead(x-2,y);
@@ -101,6 +102,42 @@ function init(){
 	snake.addHead(x,y);
 	board.render();
 	snake.render();
+	// narysować początkowego wonsza
+	// narysować planszę
+	// narysowac wonsza
 }
 
 init();
+function arrow(direction){
+	if(event.code === 'ArrowUp' && snake.direction !== "down" ) snake.direction ="up";
+	if(event.code === 'ArrowDown' && snake.direction !== "up") snake.direction ="down";
+	if(event.code === 'ArrowLeft'  && snake.direction !== "right") snake.direction = "left";
+	if(event.code === 'ArrowRight' && snake.direction !== "left") snake.direction = "right";
+	clearInterval(timeoutID)
+	step()
+
+}
+
+let gameOngoing = false;
+window.addEventListener("keydown", (e) =>{
+	if(e.code === "Space" && !gameOngoing){
+		gameOngoing = true;
+		clearTimeout(timeoutID)
+		step();
+
+	}
+	
+	this.console.log(event.code)
+	if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].includes(event.code))
+		arrow(event.code);
+});
+
+function gameover() {
+	clearTimeout(timeoutID);
+	const iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/qcSukAVDP7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	const el = document.createElement("div");
+	canvas.remove();
+	el.id = "game" ; 
+	el.innerHTML = iframe;
+	document.querySelector("#wrap").appendChild(el);
+}
